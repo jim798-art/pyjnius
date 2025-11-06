15369d0827cd2aa9e6f54e1a463ad3f43200d610
jim798-art <jim798@gmail.com>
2025-11-07 01:04:13 +0530

    Add platform-specific includes for Android

--- jnius.pyx
+++ jnius.pyx
@@ -1,5 +1,9 @@
-# Include compatibility patch
 include "jnius_utils.pxi"
+
+DEF JNIUS_PLATFORM = "android"
+
+IF JNIUS_PLATFORM == "android":
+    include "jnius_jvm_android.pxi"
 
 # Dummy function to trigger Cython build
 def dummy():
